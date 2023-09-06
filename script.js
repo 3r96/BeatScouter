1b330ded7a4060538d0890c431d5e148375b2a2c
3r96 <3r962015@gmail.com>
2023-09-06 17:02:13 -0400

    Added prev and next button functionality

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 import api from "./api.js";
 
 const currentLocation = document.querySelector(".current_location");
-const performanceNodeList = document.querySelectorAll(".performance_info_row");
-const performanceImageList = document.querySelectorAll(".performance_column");
+const performanceNodeList = document.querySelectorAll(".performance_info");
+const performanceImageList = document.querySelectorAll(".performance_img");
+const prev_button = document.querySelector(".prev_button");
+const next_button = document.querySelector(".next_button");
 
 //////////////////////////
 const clientId = api.client_id;
 /////////////////////////
 
 console.log(performanceNodeList);
+console.log(performanceImageList);
 
-const getEvents = async function () {
+const getEvents = async function (page = 1) {
   try {
     // defining parameters
     const queryParams = new URLSearchParams({
@@ -20,6 +23,7 @@ const getEvents = async function () {
       range: "50mi",
       client_id: clientId,
       //postal_code: 10001,
+      page: page,
     });
 
     const url = `https://api.seatgeek.com/2/events?${queryParams}`;
@@ -34,69 +38,159 @@ const getEvents = async function () {
 };
 
 ///////////////////// MARKUPS ////////////////////
-const infoMarkup = (title, venue, date) => {
+function infoMarkup(title, venue, date) {
   return `
     <div class="performance_title">${title}</div>
     <h1>${venue}</h1>
-    <h1>${date}</h1>
+    <h1>${date} </h1>
   `;
-};
-console.log(getEvents());
+}
+//console.log(getEvents());
 
-getEvents().then((data) => {
-  // checking if there is data coming from getEvents()
-  if (data && data.events) {
-    // save the events as an array of objects
-    const events = data.events;
+async function displayData(page) {
+  try {
+    const data = await getEvents(page);
+    if (data && data.events) {
+      const events = data.events;
+      console.log(`Your City: ${data.meta.geolocation.city}`);
 
-    // getting the city based on goeolaction
-    console.log(`Your City: ${data.meta.geolocation.city}`);
+      // clearing the current content
+      performanceNodeList.forEach((el) => {
+        el.innerHTML = "";
+      });
 
-    const locationMarkup = `
+      performanceImageList.forEach((el) => {
+        el.innerHTML = "";
+      });
+      currentLocation.innerHTML = "";
+
+      const locationMarkup = `
     <h2 class="header__location">
           Your location: 📌 <span class="highlight">${data.meta.geolocation.city}</span>
-        </h2> 
+        </h2>
     `;
-    currentLocation.insertAdjacentHTML("afterbegin", locationMarkup);
+      currentLocation.insertAdjacentHTML("afterbegin", locationMarkup);
 
-    // iterating thru the nodelist and the events object
-    performanceNodeList.forEach(function (el, index) {
-      if (index < events.length) {
-        const title = events[index].performers[0].name;
-        const venue = events[index].venue.name;
-        console.log(venue);
-        const datetime = new Date(events[index].datetime_utc);
-        const options = {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-        };
+      // iterating thru the nodelist and the events object
+      performanceNodeList.forEach(function (el, index) {
+        if (index < events.length) {
+          const title = events[index].performers[0].name;
+          const venue = events[index].venue.name;
+          console.log(venue);
+          const datetime = new Date(events[index].datetime_utc);
+          const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+          };
 
-        // datetime for the event
-        const formattedDatetime = datetime.toLocaleDateString("en-US", options);
+          // datetime for the event
+          const formattedDatetime = datetime.toLocaleDateString(
+            "en-US",
+            options
+          );
 
-        // const performerImg = events[index].performers[0].image;
+          // const performerImg = events[index].performers[0].image;
 
+          el.insertAdjacentHTML(
+            "afterbegin",
+            infoMarkup(title, venue, formattedDatetime)
+          );
+
+          // el.insertAdjacentHTML("afterbegin", `<img src=${performerImg}></img.`);
+        }
+      });
+      performanceImageList.forEach(function (el, index) {
+        const img = events[index].performers[0].image;
         el.insertAdjacentHTML(
           "afterbegin",
-          infoMarkup(title, venue, formattedDatetime)
+          `<img src=${img} class=performer_img></img.`
         );
-
-        // el.insertAdjacentHTML("afterbegin", `<img src=${performerImg}></img.`);
-      }
-    });
-    performanceImageList.forEach(function (el, index) {
-      const img = events[index].performers[0].image;
-      el.insertAdjacentHTML(
-        "afterbegin",
-        `<img src=${img} class=performer_img></img.`
-      );
-    });
-  } else {
-    console.log("No events data found.");
+      });
+    } else {
+      console.log("No events data found.");
+    }
+  } catch (error) {
+    console.log(error);
   }
+}
+
+let currentPage = 1;
+displayData(currentPage);
+next_button.addEventListener("click", function () {
+  displayData(++currentPage);
 });
+prev_button.addEventListener("click", function () {
+  if (currentPage == 1) {
+    return;
+  }
+  displayData(--currentPage);
+});
+
+/////////////////////////////////////////////////// Using
+// getEvents().then((data) => {
+//   // checking if there is data coming from getEvents()
+//   if (data && data.events) {
+//     // save the events as an array of objects
+//     const events = data.events;
+
+//     // getting the city based on goeolaction
+//     console.log(`Your City: ${data.meta.geolocation.city}`);
+
+//     const locationMarkup = `
+//     <h2 class="header__location">
+//           Your location: 📌 <span class="highlight">${data.meta.geolocation.city}</span>
+//         </h2>
+//     `;
+//     currentLocation.insertAdjacentHTML("afterbegin", locationMarkup);
+
+//     // iterating thru the nodelist and the events object
+//     performanceNodeList.forEach(function (el, index) {
+//       if (index < events.length) {
+//         const title = events[index].performers[0].name;
+//         const venue = events[index].venue.name;
+//         console.log(venue);
+//         const datetime = new Date(events[index].datetime_utc);
+//         const options = {
+//           year: "numeric",
+//           month: "long",
+//           day: "numeric",
+//           hour: "2-digit",
+//           minute: "2-digit",
+//         };
+
+//         // datetime for the event
+//         const formattedDatetime = datetime.toLocaleDateString("en-US", options);
+
+//         // const performerImg = events[index].performers[0].image;
+
+//         el.insertAdjacentHTML(
+//           "afterbegin",
+//           infoMarkup(title, venue, formattedDatetime)
+//         );
+
+//         // el.insertAdjacentHTML("afterbegin", `<img src=${performerImg}></img.`);
+//       }
+//     });
+//     performanceImageList.forEach(function (el, index) {
+//       const img = events[index].performers[0].image;
+//       el.insertAdjacentHTML(
+//         "afterbegin",
+//         `<img src=${img} class=performer_img></img.`
+//       );
+//     });
+//   } else {
+//     console.log("No events data found.");
+//   }
+// });
+
+// async function handleNextPageClick(){
+//   currentPage++;
+//   try{
+//     const eventsData = await getEvents(currentPage);
+//   }
+// };
 
 // implement rendering of the data into the html
