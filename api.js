69f8a0a9b873cd17c9ee80d00d1485b1a026dc6f
3r96 <3r962015@gmail.com>
2023-11-02 23:26:40 -0400

    castor came to save me

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
 const api = {
   client_id: "MzQ0ODI3Mzd8MTY4NzU1NzczNi45MjU1Nzk1",
+
+  spotify_client_id: "6561464820c0451da5b289348bb1b95e",
+  spotify_client_secret: "23bbd5190c784f05a0502fd3600272df",
 };
 
 export default api;
--- src/js/views/concertsView.js
+++ src/js/views/concertsView.js
@@ -1,10 +1,30 @@
+import * as model from "../model.js";
+
 class ConcertsView {
   #currentLocation = document.querySelector(".current_location");
   #performanceNodeList = document.querySelectorAll(".performance_info");
   #performanceImageList = document.querySelectorAll(".performance_img");
+  #concertsFoundMessage = document.querySelector(".found_concerts");
+  #results = document.querySelector(".results");
+  #spinnerContainer = document.querySelector(".spinner-container");
+  #spotifyPlaylist = document.querySelector(".spotify_playlist");
+  #performanceColumn = document.querySelector(".performance_column");
+  #priceTable = document.querySelector(".price_table_section");
+  #venuesMap = document.querySelector(".venues_map_section");
 
+  renderUserLocation(data) {
+    // City location markup
+
+    const locationMarkup = `
+    <h2 class="header__location">
+          Your location: 📌 <span class="highlight">${data.meta.geolocation.city}</span>
+        </h2>
+    `;
+    // adding html for current city
+    this.#currentLocation.insertAdjacentHTML("afterbegin", locationMarkup);
+  }
   // this render method accepts the data coming from the controller to render it
-  render(data) {
+  renderEvents(data) {
     const events = data.events;
 
     // clearing the current content
@@ -14,16 +34,12 @@ class ConcertsView {
     this.#performanceImageList.forEach((el) => {
       el.innerHTML = "";
     });
-    this.#currentLocation.innerHTML = "";
+    this.#spinnerContainer.classList.remove("hidden");
+    setTimeout(() => {
+      this.#spinnerContainer.classList.add("hidden");
+      this.#results.classList.remove("hidden");
+    }, 2000);
 
-    // City location markup
-    const locationMarkup = `
-    <h2 class="header__location">
-          Your location: 📌 <span class="highlight">${data.meta.geolocation.city}</span>
-        </h2>
-    `;
-    // adding html for current city
-    this.#currentLocation.insertAdjacentHTML("afterbegin", locationMarkup);
     // iterating performance Image Node list to display pics
     this.#performanceImageList.forEach(function (el, index) {
       const img = events[index].performers[0].image;
@@ -32,7 +48,7 @@ class ConcertsView {
         `<img src=${img} class=performer_img>`
       );
     });
-
+    //console.log(this.#performanceNodeList);
     // iterating performance info node list to display info for the concert
     this.#performanceNodeList.forEach(function (el, index) {
       const title = events[index].performers[0].name;
@@ -56,9 +72,157 @@ class ConcertsView {
       el.insertAdjacentHTML("afterbegin", infoMarkup);
     });
   }
-  renderCity(data) {
-    // implement rendering city in this function.
-    // Include an alert like... Events found near {city}
+  renderConcertsFoundMessage(data) {
+    this.#concertsFoundMessage.innerHTML = "";
+    // concerts found markup
+    const concertsFoundMarkup = `<h1 class="found-concerts">✅Concerts found near ${data.meta.geolocation.city}!</h1>`;
+    this.#concertsFoundMessage.insertAdjacentHTML(
+      "afterbegin",
+      concertsFoundMarkup
+    );
+  }
+  renderInvalidZipCode() {
+    this.#concertsFoundMessage.innerHTML = "";
+    // concerts found markup
+    const concertsFoundMarkup = `<h1 class="found-concerts" style="background-color: #e88d8d; color: #5d1212;">🚨Invalid zip code</h1>`;
+    this.#concertsFoundMessage.insertAdjacentHTML(
+      "afterbegin",
+      concertsFoundMarkup
+    );
+  }
+  renderEmbeddedPlaylist(data) {
+    this.#spotifyPlaylist.innerHTML = "";
+    if (data) {
+      console.log(data);
+      this.#spotifyPlaylist.insertAdjacentHTML(
+        "afterbegin",
+        `
+      <iframe src="https://open.spotify.com/embed/playlist/${data.playlists.items[0].id}?utm_source=generator" width="900" height="700" border="none" allow="geolocation" frameborder="0" allow="encrypted-media" ></iframe>
+
+    `
+      );
+      return console.log(data.playlists.items[0].id);
+    }
+  }
+  formatDate(date) {
+    const datetime = new Date(date);
+    const options = {
+      // year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+
+    // datetime for the event
+    const formattedDatetime = datetime.toLocaleDateString("en-US", options);
+    return formattedDatetime;
+  }
+  formatPrice(price) {
+    if (price) {
+      const formattedPrice = price.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+      });
+      return formattedPrice;
+    } else {
+      return `N/A`;
+    }
+  }
+  renderPriceTable(data) {
+    this.#priceTable.innerHTML = "";
+    var artists = [
+      {
+        name: "Artists",
+        minPrice: "Minimum Prices",
+        maxPrice: "Highest Prices",
+      },
+      {
+        name: `${data.events[0].performers[0].name} - ${this.formatDate(
+          data.events[0].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[0].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[0].stats.highest_price),
+      },
+      {
+        name: `${data.events[1].performers[0].name} - ${this.formatDate(
+          data.events[1].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[1].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[1].stats.highest_price),
+      },
+      {
+        name: `${data.events[2].performers[0].name} - ${this.formatDate(
+          data.events[2].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[2].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[2].stats.highest_price),
+      },
+      {
+        name: `${data.events[3].performers[0].name} - ${this.formatDate(
+          data.events[3].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[3].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[3].stats.highest_price),
+      },
+      {
+        name: `${data.events[4].performers[0].name} - ${this.formatDate(
+          data.events[4].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[4].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[4].stats.highest_price),
+      },
+      {
+        name: `${data.events[5].performers[0].name} - ${this.formatDate(
+          data.events[5].datetime_utc
+        )}`,
+        minPrice: this.formatPrice(data.events[5].stats.lowest_price),
+        maxPrice: this.formatPrice(data.events[5].stats.highest_price),
+      },
+    ];
+
+    var table = document.createElement("table");
+    var tableBody = document.createElement("tbody");
+
+    for (var i = 0; i < artists.length; i++) {
+      var row = document.createElement("tr");
+
+      var nameCell = document.createElement("td");
+      var minPriceCell = document.createElement("td");
+      var maxPriceCell = document.createElement("td");
+
+      nameCell.textContent = artists[i].name;
+      minPriceCell.textContent = artists[i].minPrice;
+      maxPriceCell.textContent = artists[i].maxPrice;
+
+      row.appendChild(nameCell);
+      row.appendChild(minPriceCell);
+      row.appendChild(maxPriceCell);
+
+      tableBody.append(row);
+    }
+
+    table.appendChild(tableBody);
+    this.#priceTable.appendChild(table);
+
+    // console.log(data.events[0].performers[0].name);
+    // console.log(data.events[0].stats.lowest_price);
+  }
+  renderVenuesMap(data) {
+    //markers for venues
+    const markers = [];
+
+    // Storing the venue name and location in an array of objects
+    for (let i = 0; i < data.events.length; i++) {
+      const venue = {
+        name: data.events[i].venue.name,
+        lat: data.events[i].venue.location.lat,
+        lon: data.events[i].venue.location.lon,
+      };
+      markers.push(venue);
+    }
+    console.log(markers);
+
+    model.initMap(markers);
   }
 }
 
